16b72ddff27ef0b1393d9dc763b74d14ccd7045f
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-01-12 23:46:13 +0530

    Update intro.java

--- intro.java
+++ intro.java
@@ -3,3 +3,13 @@ public class javabasics{
         System.out.print("Hi!");                   //print function , ';' is called statement terminator
     }
 }
+// JAVA uses both compiler (javac) and interpretor (JVM).
+
+//java source code is saved with .java extension.
+// '.java ' file is converted to ' .class ' file by javac.
+
+/* .class --> It is also called byte code. 
+This file can run on any Operating system without any recompilation or any changes, unlike .exe file in C++ which can't be executed on different Operating Systems.
+This byte code makes JAVA a more secure language. */
+
+// .java file is executed by JVM and .exe file is generated.
