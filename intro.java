4f2207a7531d9642e2771bd29bc65b059ddbb21a
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-01-12 23:02:39 +0530

    a basic intro to java

---
+++ intro.java
@@ -0,0 +1,5 @@
+public class javabasics{
+    public static void main(string args[]){
+        System.out.print("Hi!");                   //print function , ';' is called statement terminator
+    }
+}
