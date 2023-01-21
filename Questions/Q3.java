93f080bd358b2557c33387e2f616f566b3777355
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-01-21 10:53:20 +0530

    To find whether a number is prime or not

--- Questions/Q3.java
+++ Questions/Q3.java
@@ -1,4 +1,6 @@
 //To find whether a number is Prime or Not.
+
+/*Method 1*/
 import java.util.Scanner;
 
 class Prime {
@@ -26,5 +28,29 @@ class Prime {
                return  false;
        }
        return true;
+    }
+}
+
+
+/*Method 2*/
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=2;i<=n/2;i++) {
+            if(n%i == 0) {
+                System.out.println("Not Prime Number");
+                System.exit(0);
+            }
+        }
+        System.out.println("Prime Number");
+        sc.close();
     }
 }
