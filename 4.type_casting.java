6097305d00b619811f46356aed705890bee9a66f
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-01-25 23:55:01 +0530

    Typecasting , Type Promotion

--- 4.type_casting.java
+++ 4.type_casting.java
@@ -7,5 +7,23 @@ System.out.println(b);    // Gives 30 as output  --> No round off will be done .
 char ch ='a';
 int number =ch;   
 System.out.println(number);  //  Gives 97  
-/*  Why it gave 97 as output. It must have given ERROR in output, as no typecasting is being done here
+/*  🔥DOUBT! -->  Why it gave 97 as output. It must have given ERROR in output, as no typecasting is being done here
 Unlike the previous case  */
+
+
+
+/* Type Promotion */
+
+public class Typeconversion{
+  public static void main(String args[]){
+    char a ='a';
+    char b='b';
+    
+    char c= a-b;   /*Concept 1*/   // --> This line gives error   
+    
+    System.out.println(b-a);  /*Concept 2*/   // --> This gives 1 as output
+    /*  If there is a expression (addition, multiplication etc) in print statement and the datatype of operand is byte or short or char then it it promoted to int datatype
+        This is called --> Type Promotion*/
+    /* Same is the reason for the error in line 20 where (a-b) has now been promoted to int datatype and we are trying to assign its value to c which is of char datatype. */
+  }
+}
