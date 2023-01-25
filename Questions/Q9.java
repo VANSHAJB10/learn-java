bb97aa268c94bb4243d89a96996fd69089c37e88
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-01-25 23:16:39 +0530

    Update Q9.java

--- Questions/Q9.java
+++ Questions/Q9.java
@@ -13,10 +13,16 @@ public class Swapping2Numbers{
     a=b;
     b=temp;
     
-    // Logic 2
+    // Logic 2 - Using 2 variables ONLY!
     a=a+b;
     b=a-b;  /* in this a can be substitued as (a+b) (taking value of a from previous line)eventually making the expression as --> b=a */
     a=a-b;  /* in this b can be substitued as (a-b) (taking value of b from previous line) eventually making the expression as --> a=b */
+    
+    // Logic 3 - Using 2 variables ONLY!
+    a=a*b;     /* ****Values of a and b can not be zero in this logic****   --> It will give Exception if any of the value is zero*/
+    b=a/b;
+    a=a/b;
+
     
     System.out.println("After swapping a=" + a + "and b=" + b); // String Concatenation is used here
   }
