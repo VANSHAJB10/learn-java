e2d2a314c30fada69d3e0b768bb799559315d326
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-01-25 23:12:22 +0530

    Swapping 2 Numbers

--- Questions/Q9.java
+++ Questions/Q9.java
@@ -6,11 +6,20 @@ public class Swapping2Numbers{
     
     System.out.println("Before swapping a=" + a + "and b=" + b);
     
-    //Swapping logic starts here
+    /* Swapping logic starts here */
+    
+    // Logic 1
     temp=a;
     a=b;
     b=temp;
     
+    // Logic 2
+    a=a+b;
+    b=a-b;  /* in this a can be substitued as (a+b) (taking value of a from previous line)eventually making the expression as --> b=a */
+    a=a-b;  /* in this b can be substitued as (a-b) (taking value of b from previous line) eventually making the expression as --> a=b */
+    
     System.out.println("After swapping a=" + a + "and b=" + b); // String Concatenation is used here
   }
 }
+
+
