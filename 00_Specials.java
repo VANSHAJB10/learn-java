9d0da853e925e73a83c30d94fa1dd06e3e87bd43
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-01-28 21:56:05 +0530

    Create 00_Specials.java

---
+++ 00_Specials.java
@@ -0,0 +1,7 @@
+/* Q1 -->  Will the following statement give any error in Java?
+int $ = 24 */
+Ans --->  No, the statement will not give any error.
+Names of variables are called identifiers in Java. Identifier rule says, identifiers can start with
+any alphabet or underscore (“_”) or dollar (“$”).
+
+  /* Q2 --> */
