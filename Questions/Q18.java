d66eacf2d539dfd8e1dc4caf3dd5bb6a17908b7d
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-01-30 23:24:52 +0530

    Create Q18.java

---
+++ Questions/Q18.java
@@ -0,0 +1,4 @@
+/*Alice is asked to implement a Java program in which 3 integers X, Y are taken as input and all the numbers between X and Y (excluding X and Y) 
+must be added and the sum is displayed as output. 
+
+If there is no number in between, in cases like Y is greater than X or both are equal, etc., NO OUTPUT should be displayed.*/
