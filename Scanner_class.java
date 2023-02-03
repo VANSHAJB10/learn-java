e7329e7b2917981bcd17b750df63ac49207a2956
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-02-03 23:54:45 +0530

    Scanner class in java

---
+++ Scanner_class.java
@@ -0,0 +1,13 @@
+/*  1  */
+Scanner sc = new Scanner(System.in);
+String myString = sc.next();
+int myInt = sc.nextInt();
+sc.close();
+
+System.out.println("myString is: " + myString);
+System.out.println("myInt is: " + myInt);
+
+/* This code creates a Scanner object named sc and uses it to read a String and an int. 
+It then closes the sc object because there is no more input to read, and prints to stdout using System.out.println(String).*/
+
+/*   2   */
