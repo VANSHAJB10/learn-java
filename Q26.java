0ce864852f1d9db96f7124a4f3b44be29bb657d4
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-02-03 23:02:50 +0530

    logical operators | loops

--- Q26.java
+++ Q26.java
@@ -18,19 +18,18 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         int k = -1;
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
             if((n>=0)&&(n<99)){
                 for(int i=0;i<=n;i++){
                     k++;
-                        System.out.println(k);
+                    System.out.println(k);
                 }
                 System.out.println("Games End");
             }
-        else{
-            System.out.println("Invalid Input");
+           else{
+               System.out.println("Invalid Input");
         }
     }
 }
