806c5867c17447674d8a0d58dda1f75db58a7b7e
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-02-03 23:06:02 +0530

    Switch Case

--- Questions/Q25.java
+++ Questions/Q25.java
@@ -30,9 +30,10 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc = new Scanner(System.in);
-        String state =sc.nextLine();
+        String state =sc.nextLine(); /*Taking input from user*/
+        
+        /*Using switch-case statemets */
         switch(state){
             case "Assam":
                 {
