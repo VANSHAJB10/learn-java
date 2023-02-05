4a85ba29546e16537017eec66b7bc623465c2e79
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-02-05 17:36:34 +0530

    Print star pattern

---
+++ Loops.java
@@ -0,0 +1,30 @@
+/*
+
+To print this pattern:
+*
+**
+***
+****
+
+
+--> outer loop = 4 times (no of lines)
+--> inner loop for 'i'th line runs for i times (no of stars in each line)
+--> print * each time
+*/
+
+public class Solution{
+public static void main(String args[])
+{
+for (int line=1; line=<4 ; line++)
+  {
+  for(int star=1; stra<=line; star++)
+    {
+    system.out.print("*"); //NOT println
+    }
+    System.out.println():
+  }
+ }
+}
+
+
+
