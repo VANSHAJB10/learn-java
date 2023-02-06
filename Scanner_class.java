5ac1cd4751650e68ace6409eb9d30bfb2da9b75b
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-02-06 23:58:49 +0530

    Update Scanner_class.java

--- Scanner_class.java
+++ Scanner_class.java
@@ -11,6 +11,7 @@ System.out.println("myInt is: " + myInt);
 It then closes the sc object because there is no more input to read, and prints to stdout using System.out.println(String).
 **** For releasing input stream reference (System.in), scanner should be closed.****       */
 
+
 /*   2   */
 /* input 10 numbers and then print the sum */
 public void calculateSum() {
@@ -20,4 +21,29 @@ public void calculateSum() {
       sum=sum+(sc.nextInt()); //no need for a one more variable
    System.out.println("The sum is: "+sum);
   sc.close();
+}
+
+
+/*   3   */
+/*Taking different types of inputs using Scanner class*/
+import java.util.Scanner;
+
+public class Scannerinputs {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        //Reads a String value from the user
+        String str = scan.nextLine();
+
+        //Reads a double value from the user
+        double db = scan.nextDouble();
+
+        //Reads a boolean value from the user
+        boolean bool = scan.nextBoolean();
+
+        //Reads a float value from the user
+        float fl = scan.nextFloat();
+    }
 }
