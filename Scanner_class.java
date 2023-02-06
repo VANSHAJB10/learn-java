01f4ee846584b6394329c5ca6c97b82ae0f8cf23
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-02-06 23:51:15 +0530

    Update Scanner_class.java

--- Scanner_class.java
+++ Scanner_class.java
@@ -8,7 +8,8 @@ System.out.println("myString is: " + myString);
 System.out.println("myInt is: " + myInt);
 
 /* This code creates a Scanner object named sc and uses it to read a String and an int. 
-It then closes the sc object because there is no more input to read, and prints to stdout using System.out.println(String).*/
+It then closes the sc object because there is no more input to read, and prints to stdout using System.out.println(String).
+**** For releasing input stream reference (System.in), scanner should be closed.****       */
 
 /*   2   */
 /* input 10 numbers and then print the sum */
