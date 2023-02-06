07326b4cdcc248f9b8492dcfc90b2fd867428895
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-02-06 23:20:18 +0530

    Update 00_Specials.java

--- 00_Specials.java
+++ 00_Specials.java
@@ -4,4 +4,17 @@ Ans --->  No, the statement will not give any error.
 Names of variables are called identifiers in Java. Identifier rule says, identifiers can start with
 any alphabet or underscore (“_”) or dollar (“$”).
 
-  /* Q2 --> */
+/* Q2 --> */
+class Test   
+{  
+    public static void main (String args[])   
+    {  
+        System.out.println(10 + 20 + "Java");   // 30Java
+      /*  0 and 20 are treated as numbers and added to be 30. Now, their sum 30 is treated as the string and concatenated with the string Java. */
+      
+        System.out.println("Java" + 10 + 20);   //Java1020
+      /* The string Java is concatenated with 10 to be the string Java10 which will then be concatenated with 20 to be Java1020. */ 
+    }  
+} 
+
+/* Q3 -->*/
