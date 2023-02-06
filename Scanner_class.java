3638c44329a50c60e7514103765297f9b5bee48d
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-02-06 23:49:27 +0530

    Update Scanner_class.java

--- Scanner_class.java
+++ Scanner_class.java
@@ -11,3 +11,12 @@ System.out.println("myInt is: " + myInt);
 It then closes the sc object because there is no more input to read, and prints to stdout using System.out.println(String).*/
 
 /*   2   */
+/* input 10 numbers and then print the sum */
+public void calculateSum() {
+   Scanner sc=new Scanner(System.in); 
+   int sum=0;
+   for(int counter=1;counter<=10;counter++) 
+      sum=sum+(sc.nextInt()); //no need for a one more variable
+   System.out.println("The sum is: "+sum);
+  sc.close();
+}
