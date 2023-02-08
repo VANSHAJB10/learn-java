5acea9b4f5b3af4b48ef57202b7c299e3d7c255a
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-02-08 23:28:45 +0530

    Update Loops.java

--- Loops.java
+++ Loops.java
@@ -26,5 +26,21 @@ for (int line=1; line=<4 ; line++)
  }
 }
 
-
-
+/*
+Print :
+1
+12
+123
+1234   */
+public class halfpyramid {
+         
+         public static void main(String args[]){
+            int n=4;
+            for (int line=1 ; line<=n ; line++){
+                for (int number =1; number<=line;number++){
+                    System.out.print(number);
+            }
+            System.out.println();
+         }
+    }
+}
