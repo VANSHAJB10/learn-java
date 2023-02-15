42d7e3199155793aef8f4a5a1735bf24c36d6ef4
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-02-15 23:29:17 +0530

    static method

---
+++ methods.java
@@ -0,0 +1,11 @@
+/*
+ * static members have only one copy which is shared by all the objects of the
+ * class
+ * static variable/method can be accessedby using classname directly.
+ * 
+ * variable with 'static' in front of it is called "class variable".
+ * 
+ * static class is also possible but only nested classes can be static ... outer
+ * class can ever be static.
+ * static method cannot access non-static variables/members
+ */
