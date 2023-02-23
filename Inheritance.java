11d0e5bb8792f940b6d55fb01f7a22903a5201fc
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-02-23 23:48:52 +0530

    Update Inheritance.java

--- Inheritance.java
+++ Inheritance.java
@@ -1,3 +1,20 @@
+/* multiple inheritance is not allowed in java.
+ * it is resolved at runtime that method of which class will be called in case there are same method name in parent as well as child class
+ 
+ *Important*
+ Car c = new Car();  --> here c is reference variable and new Car() is object.
+
+*Focus*
+ Car cc =new Waganor();
+ cc.drive; --> drvie of Waganor() is executed
+ cc.fly(); --> generates compile timeerror
+
+ if creating a obj of child class and using ref. var. of parent class then it will not show error 
+ if  same method is present in parent and child class else it will be a compile time error
+ 
+ we cant store parent class obj in child class reference.
+ */
+
 class Car {
     void drive() {
         System.out.println("drivig Car");
