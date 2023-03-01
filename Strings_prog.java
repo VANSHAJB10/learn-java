33cb28b391d3c3d7f400a7f8ba8787feff224647
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-03-01 23:16:21 +0530

    Create Strings_prog.java

---
+++ Strings_prog.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Strings_prog{
+  public static void main (String []args){
+    /*two ways of string declaration*/
+    String str="java programming";
+    String str2 = new String("abcd");
+    
+    /*Strings are immutable*/
+   
+  }
+}
