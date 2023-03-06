1be93f6e437c278f702ed880709308fefa17b111
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-03-06 23:41:09 +0530

    Create Q43.java

---
+++ Questions/Q43.java
@@ -0,0 +1,11 @@
+public class Example {
+  public static void main(String[] args) {
+
+    String str="Hi, Welcome to google.com";
+    System.out.println("Original String:" + str);
+
+    //Using regex inside replaceAll() method to replace space with blank
+    str = str.replaceAll("\\s+", "");
+    System.out.println("String after white spaces are removed:" + str);
+  }
+}
