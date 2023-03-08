823c5cbca0be70afc75431091b7c732932bed292
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-03-08 12:06:05 +0530

    Update Q46.java

--- Questions/Q46.java
+++ Questions/Q46.java
@@ -10,3 +10,58 @@ Length of strings should be greater than 2.
 
 Output Format:
 Your program should display the “ANAGRAM” or “NOT ANAGRAM” depending on the input strings. */
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner sc= new Scanner(System.in);
+        
+        String str1 = sc.next();
+        int len1 = str1.length();
+        if (len1 < 3){
+            System.out.println("Invalid Input");
+            break;
+        }
+        else {
+            String str2 = sc.next();
+            int len2 = str2.length();
+        }
+        
+        
+        if(len2 < 3){
+            System.out.println("Invalid Input");
+        }
+        else{
+            str1 = str1.toLowerCase();
+            str2 = str2.toLowerCase();
+
+            // check if length is same
+            if(str1.length() == str2.length()) {
+
+              // convert strings to char array
+              char[] charArray1 = str1.toCharArray();
+              char[] charArray2 = str2.toCharArray();
+
+                  // sort the char array
+                    Arrays.sort(charArray1);
+                    Arrays.sort(charArray2);
+        
+                    boolean result = Arrays.equals(charArray1, charArray2);
+                    if(result) {
+                        System.out.println("ANAGRAM");
+                        }
+                    else {
+                        System.out.println("NOT ANAGRAM");
+                    }
+            }
+            else {
+                System.out.println("NOT ANAGRAM");
+                }
+            
+        }
+        
+    }
+}
