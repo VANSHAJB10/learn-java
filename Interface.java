047b83cee6f7c2f7872d975d98880a6231907b88
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-03-23 23:03:41 +0530

    Create Interface.java

---
+++ Interface.java
@@ -0,0 +1,18 @@
+*** Interface ***
+
+Multiple inheritance can be implemented using Interfaces
+
+ * we can have abstract method inside interface
+ * 
+ * if u want to give body to a method u have to make it default or static
+ * eg.  
+  default void playGame(){
+   statement 1...;
+  }
+ 
+ * They cannot be instatiated , can be only implemented by class or extended by other interfaces.
+ * Interfaces cannot have an object
+  
+ * By default variable are static and public
+ * By default methods are sttic and public
+ 
