b5a368de9805d4f5937bb448c1ad67d07180e4d4
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-03-26 23:18:37 +0530

    Update Jump_statemet.java

--- Jump_statemet.java
+++ Jump_statemet.java
@@ -33,3 +33,28 @@ public class Jump {
 		}
 	}
 }
+
+
+// 3. Continue statement inside Do-While loop    /* FOCUS!! */
+public class Jump {
+	public static void main(String[] args)
+	{
+		int i = 0;
+		do {
+			if (i == 4 || i == 18) {
+				i += 2;
+
+				// Illustrating continue statement skipping the execution when i==7 or i==15
+				continue;
+			}
+
+			// Printing to showcase continue affect
+			System.out.println(i);
+
+			// Incrementing variable by 2
+			i += 2;
+
+			// Condition check
+		} while (i <= 35);
+	}
+}
