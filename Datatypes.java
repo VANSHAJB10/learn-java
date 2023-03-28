547944db659f4e546068b6bff6de9438de73e4d5
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-03-28 23:57:16 +0530

    Update Datatypes.java

--- Datatypes.java
+++ Datatypes.java
@@ -1,5 +1,7 @@
 Two types - Primitive , Non-primitive
 
+Non-primitive data types: include Classes, Interfaces, and Arrays.
+
 primitives used to hold  *integer* values --> (byte, short, int, long):
 
 byte is an 8-bit signed integer.
@@ -8,3 +10,7 @@ int is a 32-bit signed integer.
 long is a 64-bit signed integer.
 
 size:   byte < short < int < long
+
+
+Java uses 'Unicode system' not ASCII code system.
+ * This is the reason why char uses 2 byte in java.
