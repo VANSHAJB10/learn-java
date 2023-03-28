12bdabaec3314f9cf447c0440283a6c366040381
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-03-28 23:41:02 +0530

    Create Datatypes.java

---
+++ Datatypes.java
@@ -0,0 +1,8 @@
+Two types - Primitive , Non-primitive
+
+primitives used to hold  *integer* values --> (byte, short, int, long):
+
+byte is an 8-bit signed integer.
+short is a 16-bit signed integer.
+int is a 32-bit signed integer.
+long is a 64-bit signed integer.
