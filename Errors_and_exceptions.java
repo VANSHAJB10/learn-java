f72b7656571f00de231e910fabc1e2459e5ca890
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-03-30 23:18:41 +0530

    Update Errors_and_exceptions.java

--- Errors_and_exceptions.java
+++ Errors_and_exceptions.java
@@ -10,6 +10,11 @@ Logical errors - Occurs when a prog is compiled and it runs but gives wrong outp
 Runtime errors - The program crashes abruptly while running
 They are called exceptions.
 
+Causes of runtime errors(exceptions)-->
+    1. Resource constraint
+    2. Bad input
+    3. Divide by zero
+
 *** Runtime errors must not be ignored while development of a program. They must be handled to keep up user experience and satisfaction ***
     
  */
