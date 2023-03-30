f3ac303e88bdca3effa568a9a2804fe73c2643df
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-03-30 23:07:50 +0530

    Create Errors_and_exceptions.java

---
+++ Errors_and_exceptions.java
@@ -0,0 +1,9 @@
+/* Errors -->
+Types : 
+    1. logical Errors
+    2. syntax Errors
+    3. runtime Errors 
+
+Logical errors - Occurs when a prog is compiled and it runs but gives wrong output
+     
+ */
