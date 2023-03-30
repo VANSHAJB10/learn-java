1a4e7c37dd7e8d7282e798367f936a68e0826832
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-03-30 23:43:47 +0530

    Update Errors_and_exceptions.java

--- Errors_and_exceptions.java
+++ Errors_and_exceptions.java
@@ -16,5 +16,18 @@ Causes of runtime errors(exceptions)-->
     3. Divide by zero
 
 *** Runtime errors must not be ignored while development of a program. They must be handled to keep up user experience and satisfaction ***
+
+Built-In exceptions in Java -->
+    1. NullPointerException: This is the most happening exception. This exception throws if the system expects an object but actually it was null.
+
+    2. IndexOutOfBoundsException: This exception indicates that if the index is out of range.
+
+    3. ConcurrentModificationException: If the concurrent modification happening on a method that is not permissible, then this thrown
+
+    4. UnsupportedOperationException: If the requested operation is not supported then the UnsupportedOpertionException threw.
+
+    5. ArithmeticException: Thrown if the exceptional arithmetic condition occurs
+
+    6. NumberFormatException: Thrown if the application converts a string to a numeric value, but the string doesn't have the proper format.
     
  */
