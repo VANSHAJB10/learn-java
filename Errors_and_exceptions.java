960c1da6cd9c568fc06d42fe94f493115c0e7be1
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-03-30 23:13:12 +0530

    Update Errors_and_exceptions.java

--- Errors_and_exceptions.java
+++ Errors_and_exceptions.java
@@ -1,9 +1,15 @@
-/* Errors -->
+/* 
+Errors -->
 Types : 
     1. logical Errors
     2. syntax Errors
-    3. runtime Errors 
+    3. runtime Errors ---> Exceptions
 
-Logical errors - Occurs when a prog is compiled and it runs but gives wrong output
-     
+Logical errors - Occurs when a prog is compiled and it runs but gives wrong output.
+
+Runtime errors - The program crashes abruptly while running
+They are called exceptions.
+
+*** Runtime errors must not be ignored while development of a program. They must be handled to keep up user experience and satisfaction ***
+    
  */
