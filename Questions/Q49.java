0e3b0b7ecfe03bf4e2a0a63ab83f6f7bc090f4ed
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-04-02 20:51:21 +0530

    ***  Find shortest distance ***

--- Questions/Q49.java
+++ Questions/Q49.java
@@ -59,7 +59,7 @@ public class Main {
          }
          int X2 = x*x;
          int Y2 = y*y;
-         return (float)Math.sqrt(X2 +Y2);
+         return (float)Math.sqrt(X2 +Y2);   //Dostance formula
     }
     public static void main(String[] args) {
         String path = "WNEENESENNN";
