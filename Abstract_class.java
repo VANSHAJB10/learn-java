080d7dbd466abe1a02713f381c9137ccda23ac67
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-04-04 23:55:20 +0530

    Update Abstract_class.java

--- Abstract_class.java
+++ Abstract_class.java
@@ -1,4 +1,5 @@
-/* Abstract class is a class which has abstract in front of it. It may or may not have abstract methods
+/* It shows only essential things to the user and hides the internal details.
+ *Abstract class is a class which has abstract in front of it. It may or may not have abstract methods
  * Abstract class cannot be instantiated . But it can be inherited.
  * Abstract methods are methods without body,having abstract method in front of it
  * If a class is inheriting a abstract class it requires to give definition to all its abstract methods
@@ -30,3 +31,13 @@ public class Abstract_class {
       
     
 }
+
+/*There are two ways to achieve abstraction:
+
+--> Abstract class (0 to 100%)
+--> Interface (100%)
+
+* An abstract class can have abstract and non-abstract methods.
+* It needs to be extended and its method implemented. 
+* It cannot be instantiated.
+
