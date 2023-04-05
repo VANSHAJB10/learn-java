11f86e4a94c444d5f72907fd21fa6dc76991823e
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-04-05 19:09:13 +0530

    Update Code.java

--- Java_Desktop_app/Code.java
+++ Java_Desktop_app/Code.java
@@ -8,7 +8,20 @@ Jpanel centre = new JPanel();
 JPanel centre = new JPanel();
 centre.setLayout(new GridLayout(3,3));
 
+for(){
 
+}
+f.add(center);
+
+/* 'Timer' on the top and name of player OR ' Date and Time at the bottom ' are labels*/ 
+
+JLabel date = new JLabel("Date and time",JLabel.CENTER); // to dsplay the date and time in the centre --> JLabel.CENTRE is used
+JLabel date = new JLabel("Timer");    // to dsplay the timer in the centre --> JLabel.CENTRE is used
+// *******    DEFAULT VALUE of their allignment is 'Left'    ********
+
+
+f.add(timer, BorderLayout.NORTH);     /*  to display timer at the top  -->  BorderLayout.NORTH  */
+f.add(date, BorderLayout,SOUTH);
 
 //properties of frame
 f.setSize(600,600);
