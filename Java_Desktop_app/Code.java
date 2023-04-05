f4a206eb27d097a8d0e86c979148360ac5d23dd0
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-04-05 18:55:58 +0530

    Update Code.java

--- Java_Desktop_app/Code.java
+++ Java_Desktop_app/Code.java
@@ -1,1 +1,17 @@
+mport java.awt.*;
 
+
+JFrame f= new JFrame("Puzzle game")//Frame can be created without any parameter
+Jpanel centre = new JPanel();
+
+//Panel to hold 9 button at centre of frame
+JPanel centre = new JPanel();
+centre.setLayout(new GridLayout(3,3));
+
+
+
+//properties of frame
+f.setSize(600,600);
+f.setLocationRelativeTo(null);
+f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+f.setVsible(true);
