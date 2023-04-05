b45237d6cbfe8a2154751c495ed3894cef1d900c
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-04-05 19:11:56 +0530

    Update theory.java

--- Java_Desktop_app/theory.java
+++ Java_Desktop_app/theory.java
@@ -1,3 +1,5 @@
+/*ALL constants are defined in UPPERCASE*/  
+
 JPanel p = new JPanel();
 p.setLayout(new GridLayout(0, 0));
 p.add(new JButton("1"));
@@ -9,3 +11,14 @@ p.add(new JButton("2"));
 
 /*Tic Tac Toe*/
 Panel will be havng grid layout of 3 x 3 
+
+
+/* Font is a helper class */
+Font(String name , int style, int size)
+
+there are constants defined for font style
+0 --> FONT.PLAN
+1 --> FONT.BOLD
+2 --> FONT.ITALIC
+3 --> BOLD + ITALIC
+
