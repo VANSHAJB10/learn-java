143033623384360801460c54dc9e29e4291ea5a3
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-04-05 19:23:59 +0530

    Update Code.java

--- Java_Desktop_app/Code.java
+++ Java_Desktop_app/Code.java
@@ -20,8 +20,23 @@ JLabel date = new JLabel("Timer");    // to dsplay the timer in the centre --> J
 // *******    DEFAULT VALUE of their allignment is 'Left'    ********
 
 
+// Font style
+Font font1 = new Font(Font.SERIF, Font.BOLD, 48); // type ,  style , size
+Font font2 = new Font(Font.SANS_SERIF, 3 ,36 );   // number constants can be used for font style (here -->3 = BOLD + ITALIC)
+// default font is passed in case an invalid font type is passed
+// all three paramerters are necessary to be passed to Font() and in same order (string, int , int)
+
+date.setFont(font2);  // to apply font --> .setFont() is used
+date.setFont(font1);
+
+
 f.add(timer, BorderLayout.NORTH);     /*  to display timer at the top  -->  BorderLayout.NORTH  */
 f.add(date, BorderLayout,SOUTH);
+
+LocalDate dt = LocalDate.now();   // to take local time of machine
+LocalTime lt = LocalTime.now();
+
+date.setText(dt + "\t" + lt);  // set method will set the date and tie to the label
 
 //properties of frame
 f.setSize(600,600);
