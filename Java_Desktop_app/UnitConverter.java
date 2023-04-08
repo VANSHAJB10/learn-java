c308f2b87cd3ffe3cfed873ae72061ef85c07bf1
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-04-08 11:02:12 +0530

    Create UnitConverter.java

---
+++ Java_Desktop_app/UnitConverter.java
@@ -0,0 +1,14 @@
+/*With CardLayout*/
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class CardLayoutTest {
+    public static void main(String[] rk) {
+        JFrame f = new JFrame();
+        f.setSize(600, 400);
+        f.setLocationRelativeTo(null);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        f.setVisible(true);
