be868148d146acc969f1aae1d55e52b2414d7233
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-04-08 11:31:26 +0530

    Create UnitConverter.java

---
+++ Java_Desktop_app/UnitConverter.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class UnitConverter
+{
+     public static void main(String [] rk)
+     {
+	JFrame jf = new JFrame();
+	jf.setSize(600, 400);
+	jf.setLocationRelativeTo(null);
+	jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	String [] arr = {"Temperature", "Weight", "Length", "Currency"}; // string for conversion types
+
+	
+	jf.setVisible(true); 
+      }
+}
