3818b365da84b19abf0dcc024f4a83fd53b64ec6
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-04-11 23:48:02 +0530

    Update Inheritance.java

--- Inheritance.java
+++ Inheritance.java
@@ -40,6 +40,49 @@ public class Inheritance {
     Car c = new Car();
 }
 
+
+/*2*/
+package com.company;
+
+class Base{
+    public int x;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        System.out.println("I am in base and setting x now");
+        this.x = x;
+    }
+
+    public void printMe(){
+        System.out.println("I am a constructor");
+    }
+}
+
+class Derived extends Base{
+    public int y;
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
+
+public class inheritance {
+    public static void main(String[] args) {
+        // Creating an Object of base class
+        Base b = new Base();
+        b.setX(4);
+        System.out.println(b.getX());
+    }
+}
+
+
 /*
   Method overriding - 
   Its redefining the parent class method inside child class.
