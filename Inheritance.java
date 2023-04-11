96fc21ac240701f863a8528a7141eeb9a5e92d3e
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-04-11 23:48:19 +0530

    Update Inheritance.java

--- Inheritance.java
+++ Inheritance.java
@@ -79,6 +79,11 @@ public class inheritance {
         Base b = new Base();
         b.setX(4);
         System.out.println(b.getX());
+
+   // Creating an object of derived class
+        Derived d = new Derived();
+        d.setY(43);
+        System.out.println(d.getY());
     }
 }
 
