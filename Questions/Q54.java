96c9177b7ac2b39f8a1464f6f2f72be0f066cdba
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-04-12 10:39:26 +0530

    Create Q54.java

---
+++ Questions/Q54.java
@@ -0,0 +1,33 @@
+/*
+Adwit is playing game of number division with her friend , taking 2 input numbers both are of float type only. 
+He defines rules for game is like that if second number is 0.0 then it will print Infinity and if second number is not 0.0 then it simply print its result and 
+that is also of float type.
+
+Input Format:
+In first line , enter first number. In second line , enter second number
+
+Constraints:
+Both the input numbers must be of float type
+
+Output Format:
+Output depends upon second number, if it is 0.0 then it will print Infinity , but if second number is not zero then it will print result of float division number. */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float a, b;
+
+        a = sc.nextFloat();
+        b = sc.nextFloat();
+        if(b == 0.0) {
+            System.out.println("Infinity");
+        }
+        else {
+            System.out.println(a / b);
+        }
+        sc.close();
+    }
+}
+
