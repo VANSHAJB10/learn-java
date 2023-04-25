b1dc6273fd9dae9e36aec045908577529180aacf
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-04-25 23:53:43 +0530

    Update Time_App.java

--- Java_Desktop_app/Time_App.java
+++ Java_Desktop_app/Time_App.java
@@ -26,3 +26,31 @@ public class GUI extends JFrame {
      
       // add the tabbed pane to the frame
         getContentPane().add(tabbedPane);
+      
+      // start the clock timer
+        clockTimer = new Timer(1000, new ActionListener() 
+        {
+            public void actionPerformed(ActionEvent e) {
+                clockLabel.setText(LocalTime.now().format(timeFormat));
+            }
+        });
+        clockTimer.start();
+
+        setSize(1150, 760);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+   
+   private void updateTime() {
+    String hoursStr = String.format("%02d", hours);
+    String minutesStr = String.format("%02d", minutes);
+    String secondsStr = String.format("%02d", seconds);
+    String millisecondsStr = String.format("%03d", milliseconds);
+
+    stopwatchLabel.setText(hoursStr + ":" + minutesStr + ":" + secondsStr + "." + millisecondsStr);
+}
+
+public static void main(String[] args) {
+    GUI gui = new GUI();
+ }
+}
