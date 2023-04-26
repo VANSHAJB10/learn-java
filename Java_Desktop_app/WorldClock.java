f33f0062ab7847cf866db103c83196010e8d6a90
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-04-26 23:17:38 +0530

    Update WorldClock.java

--- Java_Desktop_app/WorldClock.java
+++ Java_Desktop_app/WorldClock.java
@@ -1,1 +1,52 @@
+import java.awt.*;
+import java.awt.event.*; // for Jcomponents
+import javax.swing.*;
+import java.time.*;  // zoneid is in this package
+import java.util.*;
 
+
+class FindZoneId {
+	public static void main(String[] rk) {
+		Set<String> zones = ZoneId.getAvailableZoneIds();
+		for (String s : zones) {
+			ZoneId z = ZoneId.of(s);
+			System.out.println(s + "\t" + z);
+		}
+
+	}
+}
+
+class WorldClock {
+	static public void main(String[] rk) {
+		JFrame f = new JFrame();
+		f.setSize(1150, 760);
+		f.setLocationRelativeTo(null);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JLabel l = new JLabel("Location of Mouse", JLabel.CENTER);
+		f.add(l, BorderLayout.SOUTH);
+		l.setFont(new Font(Font.MONOSPACED, 1, 24));
+
+		JLabel jl = new JLabel(new ImageIcon("map.jpeg"));
+		f.add(jl);
+
+		class MyListener extends MouseAdapter {
+			
+			int x;
+			int y; 
+
+			public void mouseMoved(MouseEvent m) {
+
+				//to fetch x and y cordinates of mouse pointer
+				x = m.getX();
+				y = m.getY();
+        
+        
+     }
+
+		MyListener ml = new MyListener();
+		f.addMouseMotionListener(ml);
+		//f.addMouseListener(ml);
+		f.setVisible(true);
+	}
+}
