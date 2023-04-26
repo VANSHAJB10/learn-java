304a22b3f0e953ea0fae13894698bb3fe7c9b633
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-04-26 23:33:10 +0530

    Update Set_Interface.java

--- Set_Interface.java
+++ Set_Interface.java
@@ -13,6 +13,26 @@ public static void main(String args[]){
            
            System.out.println("Hashset Elements --> ");
            System.out.println(set);
+           
+            LinkedHashSet<String> linkhashset= new LinkedHashSet();
+            linkhashset.add("HS1");
+            linkhashset.add("HS2");
+            linkhashset.add("HS3");
+            linkhashset.add("HS4");
+            linkhashset.add("HS5");
             
+            System.out.println("Linked Hashset Elements --> ");
+            System.out.println(linkhashset);
+ 
+
+            TreeSet<String> treeset= new TreeSet();
+            treeset.add("TS1");
+            treeset.add("TS2");
+            treeset.add("TS3");
+            treeset.add("TS4");
+            treeset.add("TS5");
+            
+            System.out.println("Tree set Elements --> ");
+            System.out.println(treeset);
   }  
 } 
