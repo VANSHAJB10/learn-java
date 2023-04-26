7a19572e76c783b405e348c688a6e0a3a337a949
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-04-26 23:25:18 +0530

    Update Set_Interface.java

--- Set_Interface.java
+++ Set_Interface.java
@@ -1,1 +1,18 @@
+import java.util.*;  // Hashset is included in this package 
 
+class Set_Interface{  
+
+public static void main(String args[]){  
+    HashSet<String> set=new HashSet();  
+
+           set.add("Element1");    
+           set.add("Element2");  
+           set.add("Element3"); 
+           set.add("Element4"); 
+           set.add("Element5"); 
+           
+           System.out.println("Hashset Elements --> ");
+           System.out.println(set);
+            
+  }  
+} 
