86e3277e51c99412e2667911c4789e7007fab282
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-04-29 23:53:19 +0530

    Update Collections.java

--- Collections.java
+++ Collections.java
@@ -1,1 +1,6 @@
 
+
+Advantages of genric collection-->
+--> If we use the generic class, we don't need typecasting.
+--> It is type-safe and checked at compile time.
+--> Generic confirms the stability of the code by making it bug detectable at compile time
