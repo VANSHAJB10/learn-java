cf3a3ecf898319c0c0f503abb5da54eb06588ebf
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-04-29 23:57:17 +0530

    Update Collections.java

--- Collections.java
+++ Collections.java
@@ -3,7 +3,11 @@ It provides various
     'classes' such as ArrayList, Vector, Stack, and HashSet, etc. 
 and 'interfaces' such as List, Queue, Set, etc.
 
-  
+    1.1
+    Collection interface: Collection (java.util.Collection) is the primary interface, and *** every collection must implement this interface. ***
+CODE--> public interface Collection<E>extends Iterable  
+    1.2
+    
   
 Advantages of genric collection-->
 --> If we use the generic class, we don't need typecasting.
