c5c3923d8d2f652d0a2dbeac39d66563402a670e
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-04-29 23:55:00 +0530

    Update Collections.java

--- Collections.java
+++ Collections.java
@@ -1,5 +1,10 @@
+*1. Collection Framework is a combination of classes and interface, which is used to store and manipulate the data in the form of objects. 
+It provides various 
+    'classes' such as ArrayList, Vector, Stack, and HashSet, etc. 
+and 'interfaces' such as List, Queue, Set, etc.
 
-
+  
+  
 Advantages of genric collection-->
 --> If we use the generic class, we don't need typecasting.
 --> It is type-safe and checked at compile time.
