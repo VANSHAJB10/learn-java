ea37fb97cbfdaae7f35627796a19df39021d788e
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-05-01 23:55:31 +0530

    Finding number of occurences of a character in a String

--- Questions/Q57.java
+++ Questions/Q57.java
@@ -3,11 +3,26 @@ public class Main {
         public static void main(String[] args)   {
           
           String str = "Vanshajb10";
+                
+/*Approach 1*/                
           int before = str.length();
-          String str2 = str.replace("a","");
-          
-          int after = str2.length();
-          
+          String str2 = str.replace("a","");                                             // replacing the character to be counted with empty char and finding the difference in both the lengths.
+          int after = str2.length();          
+                
           System.out.println("Number of occurences of 'a' is : "+ (before-after));
+/*Approach 1 ends*/          
+                
+/*Approach 2*/ 
+           char [] ca = str.toCharArray();                                              // converting the string into a char array
+           int count = 0;
+                
+          for(int 1=0 ; i<str.length() ; i++ ){
+                if(ca[i] == 'a'){
+                    count++;  
+                }
+          }
+                
+          System.out.println("Number of occurences of 'a' is : " + count);
+/*Approach 2 ends*/                 
         }
 }
