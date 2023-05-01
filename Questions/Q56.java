013e1128519a1c94dcc23449a8b5b161856f7ce4
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-05-01 23:41:28 +0530

    remove whitespace from string

--- Questions/Q56.java
+++ Questions/Q56.java
@@ -4,6 +4,7 @@ public class Main{
   String str = "A  B  C  D";
   String str2= str.replaceAll("\\s","");    // replaceAll() method takes two parameter --> ("regex", "parameter to be replaced with")
                                                                                     // regular expression of whitespace is  -->  \\s    
+    // instead of regex as the first parameter, whitespace (" ") can also be passed 
   System.out.println(str2);
   }
 }
