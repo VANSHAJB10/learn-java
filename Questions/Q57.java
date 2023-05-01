b56aaa3fd0d3d49eeea53b66cbbbbb1db73a8bb5
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-05-01 23:57:29 +0530

    Update Q57.java

--- Questions/Q57.java
+++ Questions/Q57.java
@@ -6,11 +6,20 @@ public class Main {
                 
 /*Approach 1*/                
           int before = str.length();
+                /*Method 1*/
           String str2 = str.replace("a","");                                             // replacing the character to be counted with empty char and finding the difference in both the lengths.
-          int after = str2.length();          
+          int after = str2.length();  
+                /*Method 1 ends*/
+                
+                
+                /*Method 2*/
+          int after = (str.replace("a","")).length();
+                /*Method 2 ends*/
                 
           System.out.println("Number of occurences of 'a' is : "+ (before-after));
 /*Approach 1 ends*/          
+                
+                
                 
 /*Approach 2*/ 
            char [] ca = str.toCharArray();                                              // converting the string into a char array
