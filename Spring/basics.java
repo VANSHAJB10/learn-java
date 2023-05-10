e12d17fda0757cf1911a1ac4bebc1505f09e03e2
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-05-10 23:51:25 +0530

    Update basics.java

--- Spring/basics.java
+++ Spring/basics.java
@@ -1,1 +1,14 @@
+***  Spring Boot Features ***
 
+Web Development
+SpringApplication
+Application events and listeners
+Admin features
+Externalized Configuration
+Properties Files
+YAML Support
+Type-safe Configuration
+Logging
+Security
+
+*** *** *** *** *** *** *** ***
