62e7b59288db1da892b097fb010380d76381720d
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-05-20 23:38:47 +0530

    Update Lamda_expressions.java

--- Lamda_expressions.java
+++ Lamda_expressions.java
@@ -1,3 +1,5 @@
 /*
-**  Lambda expressions basically express instances of functional 'interfaces'.
+** Lambda expressions basically express instances of 'Functional interfaces'.     -->    (An interface with a single abstract method is called a functional interface).
+** Lamda expression = Lambda Function, which is short block of code that take parameters as input and returns a resultant value.
+** Lambda Expressions are recently included in Java SE 8. 
 */
