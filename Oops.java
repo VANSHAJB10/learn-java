ab3e3f80cf627a9f2e611e7f503eee9cd2afc0df
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-05-23 16:06:46 +0530

    Update Oops.java

--- Oops.java
+++ Oops.java
@@ -1,1 +1,7 @@
+/* Class & Object */
+* Object - real world entities
+* Class - collection of object (similar kind of objects) 
 
+/*Class*/
+* has attributes + functions 
+* name of class start with CAPITAL Letter (only a convention will not give error if not followed)
