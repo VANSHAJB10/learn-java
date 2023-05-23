a3e15a95542715d247720aac899f0ee388cda1fc
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-05-23 16:14:09 +0530

    Update Oops.java

--- Oops.java
+++ Oops.java
@@ -5,3 +5,8 @@
 /*Class*/
 * has attributes + functions 
 * name of class start with CAPITAL Letter (only a convention will not give error if not followed)
+* 'Syntax' To make an object of a class
+    Classname obj = new Classname(); // new keyword reserves a memory space in heap for the object
+* to call the function of a class:    '.'  operator is used
+* Syntax to call function of class
+    ob.func1();
