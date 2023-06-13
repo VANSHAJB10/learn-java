fbaf0c7625ec250d5f8fc339a5ba81560d58fb61
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-06-13 23:18:48 +0530

    Create Sorting.java

---
+++ Sorting.java
@@ -0,0 +1,27 @@
+/*Bubble Sort*/
+
+/*Code*/
+public class Main {
+    static void bubbleSort(int[] a){
+        int n = a.length;
+        for(int i = 0; i < n-1; i++){   
+            for(int j = 0; j < n-i-1; j++){
+                
+                if(a[j] > a[j+1]){
+                    // swap - a[j], a[j+1]
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                  }
+            }
+           
+        }
+    }
+    public static void main(String[] args) {
+        int[] a = {5, 4, 1, 2, 3, 6, 0};
+        bubbleSort(a);
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
+    }
+}
