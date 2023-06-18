60158a26e24fe887eedaf080823ead1e55001158
Vanshaj Bajaj <95224854+VANSHAJB10@users.noreply.github.com>
2023-06-18 23:54:20 +0530

    Update Linkedi_List.java

--- Linkedi_List.java
+++ Linkedi_List.java
@@ -31,7 +31,24 @@ public class Linked_List {
             //newNode will become new tail of the list    
             tail = newNode;    
         }    
-    }        
+    }   
+    //display() will display all the nodes present in the list    
+    public void display() {    
+        //Node current will point to head    
+        Node current = head;    
+            
+        if(head == null) {    
+            System.out.println("List is empty");    
+            return;    
+        }    
+        System.out.println("Nodes of singly linked list: ");    
+        while(current != null) {    
+            //Prints each node by incrementing pointer    
+            System.out.print(current.data + " ");    
+            current = current.next;    
+        }    
+        System.out.println();    
+    }         
         
     public static void main(String[] args) {    
             
@@ -43,6 +60,6 @@ public class Linked_List {
         sList.addNode(3);    
         sList.addNode(4);    
             
-           
+        sList.display();
     }    
 } 
